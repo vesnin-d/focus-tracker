bf87178bcf19c18d7a1b98365d4b191e89491a80
Denys Vesnin <dvesnin@conductor.com>
2020-06-10 01:24:18 +0300

    Add unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders welcome state', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = getByText(/Welcome/i);
+  expect(text).toBeInTheDocument();
 });
